8265bc5cb19e30e82cb06472e5ebb75817a9dae1
silversurfer5150 <willedwards@lycos.com>
2018-06-14 14:50:45 +0200

    Update Sample app

---
+++ browserAction/src/scripts/background.js
@@ -0,0 +1,3 @@
+console.log('****This is the background script executing****')
+console.log('****This is used in a situation where there is no pop-up****')
+console.log('****In this case the user just clicks the browser extension icon and something happens behind the scenes****')
--- browserAction/src/scripts/popup.js
+++ browserAction/src/scripts/popup.js
@@ -1,1 +1,17 @@
+function isFrogHidden() {
+    const frog = document.getElementById('frog')
+    const style = window.getComputedStyle(frog)
+    frog.style.display = (style.display === 'none') ? 'block' : 'none'
+}
 
+function fireContentScript() {
+    console.log('fire')
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { fireContentScript: true, type: 'fireContentScript' })
+    })
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('clickMe').addEventListener("click", isFrogHidden)
+    document.getElementById('fireContentScript').addEventListener("click", fireContentScript)
+})
